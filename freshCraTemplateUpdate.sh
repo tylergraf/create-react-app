202204ed207467ec4cd85f2cbaae4c874434c532
Joey Cozza <joeycozza@gmail.com>
2019-07-22 12:19:09 -0600

    updating the fresh-cra-template script to handle the local react-scripts file in package.json dep

--- freshCraTemplateUpdate.sh
+++ freshCraTemplateUpdate.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
+npm i -g json
 cd ${HOME}/tmp/fresh-cra-template
+
 # The order of these sed commands is kind of important. The last one of uncommenting all lines needs to occur after
 # removing comments explaining in english steps for replacing the binding path
 sed -i.bak 's/{{BINDING_PATH}}/fresh-cra-template.frontier/' blueprint.yml
@@ -8,8 +10,14 @@ sed -i.bak 's/{{BINDING_PATH}}/fresh-cra-template.frontier/' blueprint.yml
 sed -i.bak '/Replace/d' blueprint.yml
 sed -i.bak '/See/d' blueprint.yml
 sed -i.bak 's/#//' blueprint.yml
+
+NEW_CRA_VERSION=$(json -f ${TRAVIS_BUILD_DIR}/packages/react-scripts/package.json version)
+echo "NEW_CRA_VERSION: $NEW_CRA_VERSION"
+json -I -f package.json -e "this.dependencies[\"@fs/react-scripts\"]=\"$NEW_CRA_VERSION\""
+
 rm blueprint.yml.bak
 rm package-lock.json
+
 git commit -a -m 'putting correct blueprint.yml file in place'
 git remote add origin https://github.com/fs-webdev/fresh-cra-template.git
 git push --force origin master
