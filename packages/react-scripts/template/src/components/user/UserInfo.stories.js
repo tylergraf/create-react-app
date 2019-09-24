c469900a454085c813069f618c4373ecac5ec025
Joey Cozza <joeycozza@gmail.com>
2019-09-24 12:21:20 -0600

    updating storybook deps and templating

--- packages/react-scripts/template/src/components/user/UserInfo.stories.js
+++
@@ -1,9 +0,0 @@
-import { storiesOf } from '@storybook/react'
-
-import UserInfoReadme from './UserInfo.md'
-
-storiesOf('UserInfo', module).add('Default', () => null, {
-  readme: {
-    content: UserInfoReadme,
-  },
-})
