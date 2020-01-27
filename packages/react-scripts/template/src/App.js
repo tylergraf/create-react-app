f9bd1a226409cf943e4cadf72338a2273b66918b
Gabriel Dayley <gabriel.dayley@gmail.com>
2020-01-27 16:02:35 -0700

    update i18n example, add 2019 hf

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -7,7 +7,7 @@ import ErrorBoundary from '@fs/zion-error-boundary'
 // https://reactjs.org/docs/react-api.html#reactlazy
 const HomePage = React.lazy(() => import('./components/example/HomePage'))
 const UserInfo = React.lazy(() => import('./components/example/UserInfo'))
-// const I18nPage = React.lazy(() => import('./components/example/I18nPage'))
+const I18nPage = React.lazy(() => import('./components/example/I18nPage'))
 
 function App() {
   return (
@@ -15,13 +15,13 @@ function App() {
       <Tabs>
         <Tab title="Home" to="./" />
         <Tab title="User Info" to="/user" />
-        {/* <Tab title="I18n" to="/i18n" /> */}
+        <Tab title="I18n" to="/i18n" />
       </Tabs>
 
       <Switch>
         <Route exact path="/" component={HomePage} />
         <AuthRoute path="/user" component={UserInfo} />
-        {/* <Route path="/i18n" component={I18nPage} /> */}
+        <Route path="/i18n" component={I18nPage} />
         <Route component={NotFound} />
       </Switch>
     </ErrorBoundary>
--- packages/react-scripts/template/src/components/example/I18nPage.js
+++ packages/react-scripts/template/src/components/example/I18nPage.js
@@ -1,22 +1,196 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { i18n } from '@fs/zion-locale'
+import { Trans } from 'react-i18next/icu.macro'
+import { i18n, listOfMonths, listOfYears, listOfDaysInWeek, daysOfMonth, formatValue } from '@fs/zion-locale'
+import {
+  format,
+  formatDistanceStrict,
+  add,
+  sub,
+  subDays,
+  subYears,
+  setDate,
+  setDay,
+  setYear,
+  setMonth,
+  parseISO,
+  addHours,
+  addWeeks,
+  startOfWeek,
+} from '@fs/zion-locale/date-fns'
 import { css } from '@emotion/core'
-import { Body1, Cell, Grid, HeaderBlock, Separator, useAtSize, colors } from '@fs/zion-ui'
+import {
+  Body1,
+  Cell,
+  Grid,
+  HeaderBlock,
+  Separator,
+  useAtSize,
+  colors,
+  SubheaderBlock,
+  DataBlock,
+  Select,
+  InputRow,
+  Divider,
+  List,
+  ListItem,
+} from '@fs/zion-ui'
+import { Link } from '@fs/zion-router'
+
+// normally we would use lodash-es for this, but trying to reduce dependencies for demo
+const random = (lower, upper) => lower + Math.floor(Math.random() * (upper - lower + 1))
+const sample = (array) => array[Math.floor(Math.random() * array.length)]
+
+const PurposeStatement = () => {
+  const containerCss = css`
+    height: 100%;
+    color: ${colors.feedback.confirmation.accent};
+    background-color: ${colors.background.secondary};
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    padding: 50px;
+    text-align: center;
+  `
+  const highlightCss = css`
+    color: ${colors.billboard.blue20};
+  `
+
+  return (
+    <div css={containerCss}>
+      <HeaderBlock
+        size="md"
+        heading={
+          <Trans
+            i18nKey="familysearch.purpose.statement"
+            defaults="We create inspiring experiences that bring joy <span css={highlightCss}>to all people</span> as they
+            discover, gather and connect their families — past, present and future."
+            values={{ highlightCss }}
+          />
+        }
+      />
+    </div>
+  )
+}
+
+const DatePicker = () => {
+  const [t] = useTranslation()
+  const [calculatedDate, setCalculatedDate] = React.useState(new Date())
+  const currentYear = new Date().getFullYear()
+
+  return (
+    <>
+      <InputRow>
+        <Select
+          name="days-of-week"
+          label={t('common-ui:day.label', 'Day')}
+          options={listOfDaysInWeek().map((day, index) => ({
+            value: String(index),
+            label: day,
+          }))}
+          value={String(calculatedDate.getDay())}
+          onChange={(e) => setCalculatedDate(setDay(calculatedDate, e.target.value))}
+        />
+        <Select
+          name="days"
+          label={t('common-ui:day.label', 'Day')}
+          options={daysOfMonth(calculatedDate).map((day) => ({
+            value: String(day),
+            label: day,
+          }))}
+          value={String(calculatedDate.getDate())}
+          onChange={(e) => setCalculatedDate(setDate(calculatedDate, e.target.value))}
+        />
+        <Select
+          name="months"
+          label={t('common-ui:month.label', 'Month')}
+          options={listOfMonths().map((month, index) => ({
+            value: String(index),
+            label: month,
+          }))}
+          value={String(calculatedDate.getMonth())}
+          onChange={(e) => setCalculatedDate(setMonth(calculatedDate, e.target.value))}
+        />
+        <Select
+          name="years"
+          label={t('common-ui:year.label', 'Year')}
+          options={listOfYears(currentYear - 110, currentYear).map((year) => ({
+            value: String(year),
+            label: year,
+          }))}
+          value={String(calculatedDate.getFullYear())}
+          onChange={(e) => setCalculatedDate(setYear(calculatedDate, e.target.value))}
+        />
+        <Separator size="lg" />
+        <DataBlock
+          heading=""
+          data={
+            <>
+              Formatted Date:
+              <br />
+              {format(calculatedDate, 'PPPP')}
+            </>
+          }
+        />
+      </InputRow>
+    </>
+  )
+}
+
+// If you need to conditionally calculate a translation key, do not pass a calculated key name to the `t` function
+// NOT RECOMMENDED: i18n.t(`${event.type}.label`)
+// Instead, create a mapping object or mapping function that contains all the possible values specifically listed
+// and look up from that list.
+const eventLabels = {
+  BIRTH: i18n.t('birth.label', 'Birth'),
+  DEATH: i18n.t('death.label', 'Death'),
+}
 
 const I18nPage = () => {
   const [t] = useTranslation()
   const atSize = useAtSize()
+  const smallNumber = random(0, 10)
+  const largeNumber = random(1000, 10000000)
+  const yesterday = subDays(new Date(), 1)
+  const somePastDate = sub(new Date(), {
+    years: random(0, 100),
+    months: random(0, 12),
+    days: random(0, 31),
+    hours: random(0, 24),
+    minutes: random(0, 60),
+    seconds: random(0, 60),
+  })
+  const imageThirdPartyURL = 'https://billiongraves.com/'
+  const imageThirdPartyProvider = 'BillionGraves'
+  const contribution = {
+    title: 'Halifax County, North Carolina Genealogy',
+    name: 'Debbie Gurtler',
+    date: subYears(new Date(), 1),
+  }
+  const attribution = {
+    name: 'Fred Jones',
+    date: yesterday,
+  }
+  const event = {
+    type: Math.random() > 0.5 ? 'BIRTH' : 'DEATH',
+    timestamp: '1891-09-27T07:00:00.000Z',
+    place: 'Wilcox, Cochise, Arizona, United States',
+  }
+  // maintenance begins 1 hour after the start of next week
+  const maintenance = {
+    start: addHours(addWeeks(startOfWeek(new Date()), 1), 1),
+    duration: { hours: random(0, 4), minutes: sample([0, 15, 30, 45]) },
+  }
 
   return (
     <>
-      <Separator size="lg" />
+      <Separator size="sm" />
       <Grid>
         <Cell columns={atSize({ sm: 8 })}>
           <HeaderBlock
             size={atSize({ xs: 'md', md: 'lg', lg: 'xl' })}
-            heading="To All People"
-            subHeading={<div>{t('Internationalization in Frontier')}</div>}
+            heading={t('familysearch.purpose.all-people.title', 'To All People')}
+            subHeading={t('i18n-page.sub-title', 'Internationalization in Frontier')}
           />
         </Cell>
 
@@ -31,23 +205,176 @@ const I18nPage = () => {
               padding: 20px;
             `}
           >
-            <p>
-              This page supports all ???? currently supported languages using <code>setAppLocales</code>. Other pages in
-              this app only support the core 10 languages. You can use the Language menu in the footer to change the
-              language.
-              <Separator />
+            <Body1>
               The currently active locale code is <strong>{i18n.language}</strong>.
-            </p>
+            </Body1>
+            <Separator />
+            <Body1>
+              You can use the Language menu in the footer to change the language. All of the strings below should be
+              localized in our core 10 languages.
+            </Body1>
           </div>
         </Cell>
 
-        <Cell columns={atSize({ md: 3 })}>
-          <Body1>Examples:</Body1>
+        <Cell>
+          <PurposeStatement />
         </Cell>
 
-        <Cell columns={atSize({ md: 3 })} />
+        <Cell>
+          <HeaderBlock heading="Simple Strings" size="sm" />
+          <Divider />
 
-        <Cell columns={atSize({ md: 3 })} />
+          <Cell>
+            <Body1>{t('try.new.experience.feedback', 'Please try the new experience, and give feedback.')}</Body1>
+            <HeaderBlock size="xs" heading={t('nav.overview', 'Overview')} />
+            <Body1>{t('error.try.again.message', 'An error occurred. Please try again.')}</Body1>
+            <Link to="sample-images">{t('sample.images', 'Sample Images')}</Link>
+            <Body1>
+              {t(
+                'error.message.body',
+                'An error has occurred that may affect your ability to use this page. FamilySearch has been notified.'
+              )}
+            </Body1>
+          </Cell>
+
+          <HeaderBlock heading="Interpolation" size="sm" />
+          <Divider />
+
+          <Cell>
+            <Body1>
+              <Trans
+                i18nKey="changes.feedback"
+                defaults="We've made a few changes to give you a better experience. We'd love to hear your <Link to='feedback'>feedback</Link> for further improvements."
+              />
+            </Body1>
+            <Body1>
+              {t('modified.attribution', 'Modified: {date, date} by {user}', {
+                date: attribution.date,
+                user: attribution.name,
+              })}
+            </Body1>
+            <Body1>
+              {t(
+                'app.unavailable.alert',
+                '{app} will be unavailable as we make improvements to the site starting {weekday}, {month} {ordinalDayOfMonth} at {time} (UTC{utcOffset}) and lasting about {duration}.  We apologize for the inconvenience.',
+                {
+                  weekday: listOfDaysInWeek()[maintenance.start.getDay()],
+                  month: listOfMonths()[maintenance.start.getMonth()],
+                  ordinalDayOfMonth: format(maintenance.start, 'do'),
+                  time: format(maintenance.start, 'p'),
+                  utcOffset: format(new Date(), 'xxx'),
+                  duration: formatDistanceStrict(maintenance.start, add(maintenance.start, maintenance.duration)),
+                  app: 'Frontier',
+                }
+              )}
+            </Body1>
+            <Body1>
+              <Trans
+                i18nKey="image.permissions.action"
+                defaults="At <a target='_blank' href={imageThirdPartyURL} itemProp='image'>{imageThirdPartyProvider}</a>. By clicking here you will be leaving FamilySearch.org (fees and other terms may apply)."
+                values={{
+                  imageThirdPartyURL,
+                  imageThirdPartyProvider,
+                }}
+              />
+            </Body1>
+
+            <List>
+              <ListItem
+                primaryText={t(
+                  'pending.members',
+                  '{count, plural, =0 {No Pending Members} one {# Pending Member} other {# Pending Members}}',
+                  { count: 0 }
+                )}
+              />
+              <ListItem
+                primaryText={t(
+                  'pending.members',
+                  '{count, plural, =0 {No Pending Members} one {# Pending Member} other {# Pending Members}}',
+                  { count: 1 }
+                )}
+              />
+              <ListItem
+                primaryText={t(
+                  'pending.members',
+                  '{count, plural, =0 {No Pending Members} one {# Pending Member} other {# Pending Members}}',
+                  { count: 2 }
+                )}
+              />
+              <ListItem
+                primaryText={t(
+                  'pending.members',
+                  '{count, plural, =0 {No Pending Members} one {# Pending Member} other {# Pending Members}}',
+                  { count: 3 }
+                )}
+              />
+              <ListItem
+                primaryText={t(
+                  'pending.members',
+                  '{count, plural, =0 {No Pending Members} one {# Pending Member} other {# Pending Members}}',
+                  { count: 1000 }
+                )}
+              />
+            </List>
+          </Cell>
+
+          <HeaderBlock heading="Numbers" size="sm" />
+          <Divider />
+
+          <Cell>
+            <Body1>
+              {t(
+                'displaying.film.count',
+                '(This family history center has {location_count, number} of {total_count, number} films',
+                { location_count: smallNumber, total_count: largeNumber }
+              )}
+            </Body1>
+            <Body1>
+              {t('upload.progress', '{percent, number, percent} of {totalSize, number}', {
+                percent: Math.random(),
+                totalSize: largeNumber,
+              })}
+            </Body1>
+            <Body1>
+              {t('attach.error.message', '{errorCount, number} of {totalCount, number} Attachments Failed', {
+                errorCount: smallNumber,
+                totalCount: largeNumber,
+              })}
+            </Body1>
+            <Body1>{formatValue(Math.random() / smallNumber, 'percent')}</Body1>
+          </Cell>
+
+          <HeaderBlock heading="Dates & Times" size="sm" />
+          <Divider />
+
+          <Cell>
+            <DataBlock
+              heading={eventLabels[event.type]}
+              data={
+                <>
+                  {format(parseISO(event.timestamp))}
+                  <br />
+                  {event.place}
+                </>
+              }
+            />
+            <SubheaderBlock
+              size="sm"
+              heading={contribution.title}
+              subHeading={`${t('common-ui:by.contributor.label', 'by {contributorName}', {
+                contributorName: contribution.name,
+              })} | ${format(contribution.date)}`}
+            />
+            <DatePicker />
+            <Body1>{t('auto-save.label', 'Saved automatically at {time, time, short}', { time: new Date() })}</Body1>
+            <Body1>
+              {t('lesson.plan.title.placeholder', 'Lesson {number, number} ({date, date, medium})', {
+                number: smallNumber,
+                date: somePastDate,
+              })}
+            </Body1>
+          </Cell>
+        </Cell>
       </Grid>
     </>
   )
