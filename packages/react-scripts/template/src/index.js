687617c23e77869d04ba64d2f428ae20086b7225
redbugz <allredlt@familysearch.org>
2020-01-29 17:28:06 -0700

    Add initial Sentry support

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,25 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as Sentry from '@sentry/browser'
 import { addTranslations } from '@fs/zion-locale'
 import Root from '@fs/zion-root'
 import { Router } from '@fs/zion-router'
+import { appPath, sentryDSN, mergeSentryConfig } from '@fs/zion-config'
+
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import translations from './locales'
+
+// initialize Sentry for the app
+if (sentryDSN) {
+  // pass Sentry overrides to mergeSentryConfig
+  Sentry.init(
+    mergeSentryConfig({
+      dsn: sentryDSN,
+    })
+  )
+}
 
 // For details about loading translations: https://www.familysearch.org/frontier/docs/develop/i18n
 addTranslations(translations)
 
 // This is where you pass data from the server to the client using the SERVER_DATA global.
 // Here we pass the mounted app path from the server to the client.
-const base = window.SERVER_DATA ? new URL(window.SERVER_DATA.appPath).pathname : ''
+const base = appPath ? new URL(appPath).pathname : ''
 
 const FrontierRoot = () => (
-  <Root>
-    <Router basename={base}>
-      <App />
-    </Router>
-  </Root>
+  <React.StrictMode>
+    <Root>
+      <Router basename={base}>
+        <App />
+      </Router>
+    </Root>
+  </React.StrictMode>
 )
 
 ReactDOM.render(<FrontierRoot />, document.getElementById('root'))
