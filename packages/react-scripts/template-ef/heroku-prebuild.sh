cf1cb40fc57ecaa90b33462316d58a4fa47aea33
Joey Cozza <joeycozza@gmail.com>
2020-05-28 10:22:38 -0600

    moving template-ef and template-hf into the cra-template repo

--- packages/react-scripts/template-ef/heroku-prebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$NODE_ENV" == "production" ]; then
-  echo "-----> Building .npmrc and .netrc"
-
-  echo "//repo.churchofjesuschrist.org/artifactory/api/npm/npm-fhd/:_authToken=${NPM_TOKEN}" > .npmrc
-  echo "@fs:registry=https://repo.churchofjesuschrist.org/artifactory/api/npm/npm-fhd/" >> .npmrc
-  echo -e "machine github.com\n  login $GITHUB_AUTH_TOKEN" > ~/.netrc
-fi
