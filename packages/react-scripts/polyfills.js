17404290f7d0f1f65154b5f65e8f08a34578c7eb
Joey Cozza <joeycozza@gmail.com>
2020-06-12 14:01:58 -0600

    array.flat instead of array.find

--- packages/react-scripts/polyfills.js
+++ packages/react-scripts/polyfills.js
@@ -1,6 +1,6 @@
 'use strict'
 
-require('core-js/modules/es.array.find')
+require('core-js/modules/es.array.flat')
 require('core-js/modules/es.object.from-entries')
 require('core-js/modules/es.promise.all-settled')
 require('core-js/modules/es.promise.finally')
