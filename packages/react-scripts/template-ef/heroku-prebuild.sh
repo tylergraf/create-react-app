8a4d725f93b11de137d5e9b37c6911b7666ebbf6
Joey Cozza <joeycozza@gmail.com>
2020-07-13 13:03:44 -0600

    update to jfrog urls in v4.x branch

--- packages/react-scripts/template-ef/heroku-prebuild.sh
+++ packages/react-scripts/template-ef/heroku-prebuild.sh
@@ -3,7 +3,7 @@
 if [ "$NODE_ENV" == "production" ]; then
   echo "-----> Building .npmrc and .netrc"
 
-  echo "//repo.churchofjesuschrist.org/artifactory/api/npm/npm-fhd/:_authToken=${NPM_TOKEN}" > .npmrc
-  echo "@fs:registry=https://repo.churchofjesuschrist.org/artifactory/api/npm/npm-fhd/" >> .npmrc
+  echo "//familysearch.jfrog.io/familysearch/api/npm/fs-npm-prod-virtual/:_authToken=${NPM_TOKEN}" > .npmrc
+  echo "@fs:registry=https://familysearch.jfrog.io/familysearch/api/npm/fs-npm-prod-virtual/" >> .npmrc
   echo -e "machine github.com\n  login $GITHUB_AUTH_TOKEN" > ~/.netrc
 fi
