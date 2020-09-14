257f942d452b0859ea556a136b61fe092c1f329e
Joey Cozza <joeycozza@gmail.com>
2020-09-14 11:52:33 -0600

    adding flat-map to the polyfills

--- packages/react-scripts/polyfills.js
+++ packages/react-scripts/polyfills.js
@@ -2,6 +2,7 @@
 
 require('intersection-observer')
 require('core-js/modules/es.array.flat')
+require('core-js/modules/es.array.flat-map')
 require('core-js/modules/es.object.from-entries')
 require('core-js/modules/es.promise.all-settled')
 require('core-js/modules/es.promise.finally')
