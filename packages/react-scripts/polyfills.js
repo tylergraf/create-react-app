edb076d88bc054a6978c48ccb0a701bcb03b5237
Joey Cozza <joeycozza@gmail.com>
2020-09-14 11:51:54 -0600

    adding flat-map to the polyfills

--- packages/react-scripts/polyfills.js
+++ packages/react-scripts/polyfills.js
@@ -1,6 +1,7 @@
 'use strict'
 
 require('core-js/modules/es.array.flat')
+require('core-js/modules/es.array.flat-map')
 require('core-js/modules/es.object.from-entries')
 require('core-js/modules/es.promise.all-settled')
 require('core-js/modules/es.promise.finally')
