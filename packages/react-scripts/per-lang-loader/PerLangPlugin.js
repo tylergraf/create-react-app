e8c79bb235410edd683811b6ce8c72b08d5dc2e4
Tyler Graf <tylergraf@gmail.com>
2020-12-21 11:11:31 -0700

    Make beta release.

--- packages/react-scripts/per-lang-loader/PerLangPlugin.js
+++ packages/react-scripts/per-lang-loader/PerLangPlugin.js
@@ -1,32 +1,30 @@
+'use strict';
+
 class PerLangPlugin {
   apply(compiler) {
     const allLocales = {};
     // inject a function to loaderContext so loaders can pass back info
-    compiler.hooks.compilation.tap("PerLangPlugin", compilation =>
+    compiler.hooks.compilation.tap('PerLangPlugin', compilation =>
       compilation.hooks.normalModuleLoader.tap(
-        "PerLangPlugin",
-        (loaderContext, module) => {
+        'PerLangPlugin',
+        loaderContext => {
           loaderContext.addLocales = ({ lang, content, namespace }) => {
             try {
               allLocales[lang] = allLocales[lang] || {};
               allLocales[lang][namespace] = allLocales[lang][namespace] || {};
               allLocales[lang][namespace] = {
                 ...allLocales[lang][namespace],
-                ...content
+                ...content,
               };
             } catch (e) {
-              console.log("errorrrr");
+              console.log('errorrrr');
             }
           };
         }
       )
     );
-    // compiler.hooks.shouldEmit.tap("MyPlugin", compilation => {
-    //   // return true to emit the output, otherwise false
-    //   return true;
-    // });
     // at the end, generate an asset using the data
-    compiler.hooks.emit.tapAsync("PerLangPlugin", (compilation, callback) => {
+    compiler.hooks.emit.tapAsync('PerLangPlugin', (compilation, callback) => {
       const version = process.env.BUILD_VERSION;
       const appName = process.env.APP_NAME;
       const langs = Object.keys(allLocales);
@@ -38,7 +36,7 @@ class PerLangPlugin {
             `/static/locales/${appName}/${version}/${lang}/${namespace}.json`
           ] = {
             source: () => content,
-            size: () => content.length
+            size: () => content.length,
           };
         });
       });
